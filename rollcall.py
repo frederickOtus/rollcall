c23d1ae8b899dbeb8aa92d4cc1787f377db12eca
frederickOtus <pita.da.bread07@gmail.com>
2013-04-20 15:52:10 -0400

    renamed:    rollcall.py -> oldcall.py 	modified:   rollcall.py
    This is the version that has the benchmarking of downliading the ids vs grabbing
    them parsing all of the files

--- rollcall.py
+++ rollcall.py
@@ -1,39 +1,70 @@
 from utils import urljson_to_python as dl, get_count, purge_on_attr as purge
+import xml.etree.ElementTree as ET
+from time import time
 
-vote_url = "http://www.govtrack.us/api/v2/vote"
-key_mapping = {'+':0, '-':1, '0':2, 'P':3}
+def format_vote(filepath):
+    """Takes a filepath to a rollcall vote and returns a 1 dimensional array
+    with 5 rows, the first four are arrays of the ids of congresspersons who voted
+        -for
+        -against
+        -abstain
+        -were not present
+    The last is a dictionary of attributes
+    """
+    vote_mapping = {'+': 0, '-':1, "P":2,"0":3}
+    #this is a mapping of the symbols used to represent votes to which
+    #array we are using to track that vote type
+    extra_attrs = ['category','question','result','type']
+    #Extra nodes hat we want to pull content from
+    root = ET.parse(filepath).getroot()
+    attrs = root.attrib
+    for a in extra_attrs:
+        attrs[a] = root.find(a).text
 
-def congress_votes(congress_num):
-    url = vote_url + "?congress=%s" % (congress_num)
-    count = get_count(url)
-    url += "&limit=%s" % (count)
-    votes = dl(url)['objects']
-    return votes
+    formatted_data = [[],[],[],[],[]]
 
-def collate_voters(congress_num):
-    import time
-    t1 = time.time()
+    for voter in root.iter('voter'):
+        v_id = voter.attrib['id']
+        vote_sym = voter.attrib['vote']
+        vote_indx = vote_mapping[vote_sym]
+        formatted_data[vote_indx].append(v_id)
+
+    formatted_data[4] = attrs #the last elm is an array of attributes about the
+                              # rollcall
+
+    return formatted_data    
+
+def bench(func, *args, **kwargs):
+    t1 = time()
+    func(*args, **kwargs)
+    return time()-t1
+
+def get_specific_votes(congress_num, cats):
+    from oldcall import congress_votes
     votes = congress_votes(congress_num)
-    print len(votes)
-    atr = [{'name':'category', 'type':'in', 'value':['passage',
-        'passage_part']},]
+    atr = [{'name':'category', 'type':'in', 'value':cats},]
     purged = purge(votes, atr)
     ids = [ str(p['id']) for p in purged ]
-    id_str = '|'.join(ids)
+    return ids
 
-    count_url = "http://www.govtrack.us/api/v2/vote_voter?vote__in=%s" % (id_str)
-    count = get_count(count_url)
-    url = "http://www.govtrack.us/api/v2/vote_voter?limit=%s&vote__in=%s" % (count, id_str)
-    member_votes = dl(url)['objects']
-    collated_votes = {}
-    print len(member_votes)
-    for vote in member_votes:
-        vote_id = vote['vote']['id']
-        key = vote['option']['key']
-        member = vote['person']['id']
-        if not collated_votes.has_key(vote_id):
-            collated_votes[vote_id] = [[],[],[],[]]
-        key_index = key_mapping[key]
-        collated_votes[vote_id][key_index].append(member)
-    print "time= %s" % (time.time()-t1)
-    return collated_votes
+def get_specific_votes_v2(congress_num, cats):
+    from os import listdir
+    from os.path import isfile, join
+
+    vote_path = 'data/%s/rolls' %(congress_num)
+    def is_valid_file(f):
+        if not isfile(join(vote_path,f)):
+            return False
+        xml_root = ET.parse("%s/%s" %(vote_path, f)).getroot()
+        category = xml_root.find('category').text
+        return category in cats
+
+    files = [ f for f in listdir(vote_path) if is_valid_file(f) ]
+    return files 
+    
+if __name__ == "__main__":
+    cats = ['passage',]
+    print 'benchmarking web call'
+    print bench(get_specific_votes, 112, cats) 
+    print 'benchmarking local call'
+    print bench(get_specific_votes_v2, 112, cats) 
